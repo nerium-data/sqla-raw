a0968d138351a84faea475c2f903e39435618a59
Thomas Yager-Madden <10351894+tym-xqo@users.noreply.github.com>
2020-03-08 11:04:24 -0500

    Add jinja templating and basic test

--- db.py
+++ db.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Use SQLAlchemy engine to fetch a dataset from configured query_name
+"""Use SQLAlchemy engine to fetch a dataset from a query
 """
 import os
+
+from jinja2.sandbox import SandboxedEnvironment
 from sqlalchemy import create_engine
 
 
@@ -13,9 +15,19 @@ def connection():
     return conn
 
 
+def process_template(obj, **kwargs):
+    """Render query body using jinja2 sandbox
+    TODO: Prevent variable expansion
+    """
+    env = SandboxedEnvironment()
+    template = env.from_string(obj)
+    return template.render(kwargs)
+
+
 def result(sql, **kwargs):
     try:
         db = connection()
+        sql = process_template(sql, **kwargs)
         cur = db.execute(sql, **kwargs)
         cols = cur.keys()
         result = cur.fetchall()
@@ -23,3 +35,10 @@ def result(sql, **kwargs):
     except Exception as e:
         rows = [{"error": repr(e)}]
     return rows
+
+
+if __name__ == "__main__":
+    sql = "select 'foo' as bar"
+    result = result(sql)
+    assert result == [{"bar": "foo"}]
+    print(result)
